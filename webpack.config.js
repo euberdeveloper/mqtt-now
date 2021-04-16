c8edc6bb8e275210bbc1e621c7e5ba57b09129d3
euberdeveloper <euberdeveloper@gmail.com>
2021-04-16 18:58:28 +0200

    build: add webpack
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const DtsBundleWebpack = require('dts-bundle-webpack');
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    // devtool: 'source-map',
+    entry: {
+        index: './source/index.ts',
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                include: path.resolve(__dirname, 'source'),
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new DtsBundleWebpack({
+            name: 'mqtt-now',
+            main: 'dist/index.d.ts',
+            out: '../bundled/index.d.ts'
+        })
+    ],
+    externals: [nodeExternals()],
+    output: {
+        path: path.resolve(__dirname, './bundled'),
+        filename: 'index.js',
+        library: 'mqtt-now',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true,
+    }
+}
